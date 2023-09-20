d49eee1451117333df5cf49fa3a82ca0c56d39e4
ANDRES GARCES <ANDRES GARCES@192.168.10.17>
2023-09-19 20:52:04 -0500

    Primer ajuste en el Main

--- src/main/java/Lógica_Cliente/Main.java
+++ src/main/java/Lógica_Cliente/Main.java
@@ -18,7 +18,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        //Hola
         //Declarar variables
         String nombre = "";
         int cédula = 0;
